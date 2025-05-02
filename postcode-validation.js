9bea8a2c1e6ca1a3269ffc8fc4a3f8235e7140a2
ItzA13x <alex.vladiuc@gmail.com>
2025-05-02 18:20:08 +0100

    push

--- postcode-validation.js
+++ postcode-validation.js
@@ -63,20 +63,22 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 
+
 const menuButton = document.getElementById('menuButton');
 const mobileMenu = document.getElementById('mobileMenu');
 const [bar1, bar2, bar3] = menuButton.querySelectorAll('span');
 
+let isOpen = false;
+
 menuButton.addEventListener('click', () => {
-mobileMenu.classList.toggle('hidden');
-// Animate lines into an X
-bar1.classList.toggle('rotate-45');
-bar1.classList.toggle('translate-y-[6px]');
+    isOpen = !isOpen;
+    mobileMenu.classList.toggle('hidden');
 
-bar2.classList.toggle('opacity-0');
-
-bar3.classList.toggle('-rotate-45');
-bar3.classList.toggle('-translate-y-[6px]');
+    bar1.classList.toggle('translate-y-[6px]');
+    bar2.classList.toggle('opacity-0');
+    bar3.classList.toggle('-rotate-45');
+    bar3.classList.toggle('-translate-y-[6px]');
 });
+
 
 
